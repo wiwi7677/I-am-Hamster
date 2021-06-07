ef6a33306b61337e7598aa9f97af299db6654e7e
Wiwi <wiwi880331@gmail.com>
2021-06-08 00:56:48 +0800

    Initial Commit

---
+++ I am Hamster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Hamster
+//
+//  Created by wiwi on 2021/6/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
